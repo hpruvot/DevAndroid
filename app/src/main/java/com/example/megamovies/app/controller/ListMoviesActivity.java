6c0c9ac887d1835a383aab5ac8435e4d9529d83b
lnpruvot <helene.pruvot1@gmail.com>
2014-06-03 18:30:27 +0200

    ini

---
+++ app/src/main/java/com/example/megamovies/app/controller/ListMoviesActivity.java
@@ -0,0 +1,175 @@
+package com.example.megamovies.app.controller;
+
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.FilterQueryProvider;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.SimpleCursorAdapter;
+
+import com.example.megamovies.app.R;
+import com.example.megamovies.app.model.DB;
+import com.example.megamovies.app.model.Provider;
+
+
+public class ListMoviesActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    public final static String ID = "com.example.movies.ListMoviesActivity.ID";
+
+    private int i = 0;
+    private GridView listMovies;
+
+    private SimpleCursorAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // On desactive la sortie du clavier
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        //Récupération de la gridview créée dans le fichier main.xml
+        listMovies = (GridView) findViewById(R.id.listofmovies);
+
+        this.addMovies();
+
+        // Listener pour la liste
+        AdapterView.OnItemClickListener moviesListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent(ListMoviesActivity.this, DisplayMovieActivity.class);
+                intent.putExtra(ID, id);
+                startActivity(intent);
+            }
+        };
+
+        adapter = new SimpleCursorAdapter (this, R.layout.item_movie_list, null,
+                new String[] {DB.Film.IMAGE, DB.Film.TITLE, DB.Film.GENDER},
+                new int[] {R.id.img, R.id.title, R.id.gender}, 0);
+        adapter.setFilterQueryProvider(new FilterQueryProvider() {
+            public Cursor runQuery(CharSequence constraint) {
+                return getContentResolver().query(Provider.CONTENT_URI_SEARCH, null, constraint.toString(), null, null);
+            }
+        });
+        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder(){
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex){
+                if(view.getId() == R.id.img){
+                    if (cursor.getInt(columnIndex) != -1){
+                        ((ImageView)view).setImageResource(cursor.getInt(columnIndex));
+                    }
+                    else
+                        // TODO remplacer l'image par defaut
+                        ((ImageView)view).setImageResource(R.drawable.list_avatar);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+
+        getLoaderManager().initLoader(0, null, this);
+
+
+        //On attribue à notre listView l'adapter que l'on vient de créer
+        listMovies.setAdapter(adapter);
+
+
+        //On écoute la liste
+        listMovies.setOnItemClickListener(moviesListener);
+
+
+        //Reecherche
+
+        // On récupère la barre de recherche
+        EditText search = (EditText) findViewById(R.id.search);
+
+        // On créé un ecouteur pour la recherche
+        TextWatcher searchTextWatcher = new TextWatcher(){
+
+            public void afterTextChanged(Editable arg0) {}
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // On met à jour la liste d'item
+                adapter.getFilter().filter(s.toString());
+            }
+
+        };
+
+        // On lie la recherche au lister
+        search.addTextChangedListener(searchTextWatcher);
+    }
+
+
+    private void addMovies() {
+        /*
+        map.put("titre", "Avatar");
+        map.put("genre", "Science-fiction");
+        map.put("img", String.valueOf(R.drawable.list_avatar));
+
+        map.put("titre", "Django");
+        map.put("genre", "Western");
+        map.put("img", String.valueOf(R.drawable.list_django));
+
+        map.put("titre", "Forrest Gump");
+        map.put("genre", "Comédie, Romance");
+        map.put("img", String.valueOf(R.drawable.list_forrestgump));
+
+        map.put("titre", "Gran Torino");
+        map.put("genre", "Drame, Thriller");
+        map.put("img", String.valueOf(R.drawable.list_grantorino));
+
+        map.put("titre", "Intouchables");
+        map.put("genre", "Comédie");
+        map.put("img", String.valueOf(R.drawable.list_intouchables));
+
+        map.put("titre", "The dark knight rises");
+        map.put("genre", "Action, Thriller");
+        map.put("img", String.valueOf(R.drawable.list_thedarkknightrises));
+
+        map.put("titre", "Pulp Fiction");
+        map.put("genre", "Policier, Thriller");
+        map.put("img", String.valueOf(R.drawable.list_pulpfiction));
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("titre", "Fight Club");
+        map.put("genre", "Thriller, Drame");
+        map.put("img", String.valueOf(R.drawable.list_fightclub));*/
+
+        ContentValues movie = new ContentValues();
+        movie.put(DB.Film.TITLE, "Le seigneur des anneaux");
+        movie.put(DB.Film.GENDER, "Fantastique, Action");
+        movie.put(DB.Film.IMAGE, R.drawable.list_leseigneurdesanneaux);
+        this.getContentResolver().insert(Provider.CONTENT_URI_MOVIE_ADD, movie);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, Provider.CONTENT_URI_MOVIES, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+    }
+}
---
+++ app/src/main/java/com/example/megamovies/app/model/Provider.java
@@ -0,0 +1,230 @@
+package com.example.megamovies.app.model;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+
+public class Provider extends ContentProvider {
+	
+
+	private static final String authority = "com.example.megamovies.provider";
+
+    public static final String uriMovies = "content://"+authority+"/movies";
+    public static final String uriMovieID = "content://"+authority+"/movie";
+	private static final String uriMovieAdd = "content://"+authority+"/movie/add";
+    public static final String uriSearch = "content://"+authority+"/search";
+
+	public static final Uri CONTENT_URI_MOVIES = Uri.parse(uriMovies);
+	public static final Uri CONTENT_URI_MOVIE_ADD = Uri.parse(uriMovieAdd);
+	public static final Uri CONTENT_URI_SEARCH = Uri.parse(uriSearch);
+	
+	public static final int MOVIES = 0;
+	public static final int MOVIE_ID = 1;
+	public static final int MOVIE_ADD = 2;
+	public static final int SEARCH = 3;
+
+    private DatabaseHelper DH;
+	private SQLiteDatabase mDb;
+	
+	public static UriMatcher uriMatcher;
+	
+	static {
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(authority, "movies", MOVIES);
+		uriMatcher.addURI(authority, "movie/#", MOVIE_ID);
+		uriMatcher.addURI(authority, "movie/add", MOVIE_ADD);
+		uriMatcher.addURI(authority, "search/", SEARCH);
+	}
+	
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+/*		Log.i("Provider", "delete()");
+		int ret = 0;
+		int id = 0;
+		switch (uriMatcher.match(uri)) {
+			case CONTACT_ID:
+				Log.i("Provider", "CONTACT_ID");
+				try {
+					id = Integer.parseInt(uri.getPathSegments().get(1));
+					ret = (int) mDb.delete(DB.Contact.TABLE_NAME, "id = " + id, null);
+					getContext().getContentResolver().notifyChange(CONTENT_URI_CONTACT, null);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case CONTACT_ADRESSE:
+				Log.i("Provider", "CONTACT_ADRESSE");
+				try {
+					id = Integer.parseInt(uri.getPathSegments().get(1));
+					ret = (int) mDb.delete(DB.Adresse.TABLE_NAME, "id = " + id, null);
+					getContext().getContentResolver().notifyChange(CONTENT_URI_ADRESSE, null);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case CONTACT_MAIL:
+				Log.i("Provider", "CONTACT_MAIL");
+				try {
+					id = Integer.parseInt(uri.getPathSegments().get(1));
+					ret = (int) mDb.delete(DB.Mail.TABLE_NAME, "id = " + id, null);
+					getContext().getContentResolver().notifyChange(CONTENT_URI_MAIL, null);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case CONTACT_NUMERO:
+				Log.i("Provider", "CONTACT_NUMERO");
+				try {
+					id = Integer.parseInt(uri.getPathSegments().get(1));
+					ret = (int) mDb.delete(DB.Numero.TABLE_NAME, "id = " + id, null);
+					getContext().getContentResolver().notifyChange(CONTENT_URI_NUMERO, null);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			default:
+				throw new IllegalArgumentException("URI non support�e : "+uri);
+		}
+		return ret;
+	}
+*/
+	public Uri insert(Uri uri, ContentValues cont) {
+		Log.i("Provider", "insert()");
+		long id = 0;
+		switch (uriMatcher.match(uri)) {
+			case MOVIE_ADD:
+				Log.i("Provider", "CONTACT:");
+				id = mDb.insert(DB.Film.TABLE_NAME, null, cont);
+				if (id>0) {
+					Uri geturi = ContentUris.withAppendedId(CONTENT_URI_MOVIES, id);
+					getContext().getContentResolver().notifyChange(uri,  null);
+					Log.i("Provider", "Movie added : "+id);
+					return geturi;
+				}
+				return null;
+			default:
+				throw new IllegalArgumentException("URI non supportee : "+uri);
+		}
+	}
+
+	public boolean onCreate() {
+		Log.i("Provider", "onCreate()");
+        Context context = getContext();
+		DH = new DatabaseHelper(context);
+		mDb = DH.getWritableDatabase();
+        return (DH == null) ? false : true;
+	}
+
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		Log.i("Provider", "query()");
+		Cursor ret = null;
+		int id = 0;
+		switch (uriMatcher.match(uri)) {
+			case MOVIE_ID:
+				Log.i("Provider", "case MOVIE_ID:");
+				// Recupere l'index du contact.
+				try { 
+					id = Integer.parseInt(uri.getPathSegments().get(1));
+				} catch (Exception ex) {
+					System.out.println(ex.getMessage());
+					return null;
+				}
+				// Recupere le curseur contenant le contact.
+				ret = mDb.rawQuery("SELECT * FROM " + DB.Film.TABLE_NAME + " WHERE " + DB.Film.ID +
+						" = ?", new String[] {String.valueOf(id)});
+				// Enregistre le ContentResolver de facon a ce qu'il soit notifie si
+				// le curseur change.
+				ret.setNotificationUri(getContext().getContentResolver(), uri);
+				return ret;
+            case MOVIES:
+                Log.i("Provider", "case MOVIES:");
+                // Recupere le curseur contenant l'ensemble des entete de film.
+                ret = mDb.rawQuery("SELECT " + DB.Film.ID + " as _id," + DB.Film.TITLE + ", "  +
+                        DB.Film.YEAR + ", " + DB.Film.REALISATOR + ", " + DB.Film.AVAILABLE+ ", " +
+                        DB.Film.VIEWED + ", " + DB.Film.IMAGE + ", " + DB.Film.GENDER + " FROM " +
+                        DB.Film.TABLE_NAME + " ORDER BY " + DB.Film.TITLE + ";", null);
+
+                // Enregistre le ContentResolver de facon a ce qu'il soit notifie si
+                // le curseur change.
+                ret.setNotificationUri(getContext().getContentResolver(), uri);
+                return ret;
+            case SEARCH:
+                Log.i("Provider", "case SEARCH:");
+                // Recupere le curseur contenant l'ensemble des entete de film.
+                ret = mDb.rawQuery("SELECT " + DB.Film.ID + " as _id," + DB.Film.TITLE + ", "  +
+                        DB.Film.YEAR + ", " + DB.Film.REALISATOR + ", " + DB.Film.AVAILABLE+ ", " +
+                        DB.Film.VIEWED + ", " + DB.Film.IMAGE + ", " + DB.Film.GENDER + " FROM " +
+                        DB.Film.TABLE_NAME + " WHERE  (upper(" +
+                        DB.Film.TITLE + ") LIKE '%" + selection.toUpperCase() + "%') ORDER BY " + DB.Film.TITLE + ";", null);
+
+                // Enregistre le ContentResolver de facon a ce qu'il soit notifie si
+                // le curseur change.
+                ret.setNotificationUri(getContext().getContentResolver(), uri);
+                return ret;
+			default:
+				throw new IllegalArgumentException("URI non supportee : "+uri);
+		}
+	}
+
+	public int update(Uri uri, ContentValues cont, String condition, String[] conditionArgs) {
+        return 0;
+    }
+/*		Log.i("Provider", "update()");
+		int ret = 0;
+		String id = "";
+		switch (uriMatcher.match(uri)) {
+		case CONTACT_ID:
+			id = uri.getPathSegments().get(1);
+			ret = mDb.update(DB.Contact.TABLE_NAME, cont, "id" + " = " + id, null);
+			getContext().getContentResolver().notifyChange(CONTENT_URI_CONTACT, null);
+			break;
+		case CONTACT_ADRESSE:
+			id = uri.getPathSegments().get(1);
+			ret = mDb.update(DB.Adresse.TABLE_NAME, cont, "id" + " = " + id, null);
+			getContext().getContentResolver().notifyChange(CONTENT_URI_ADRESSE, null);
+			break;
+		case CONTACT_MAIL:
+			id = uri.getPathSegments().get(1);
+			ret = mDb.update(DB.Mail.TABLE_NAME, cont, "id" + " = " + id, null);
+			getContext().getContentResolver().notifyChange(CONTENT_URI_MAIL, null);
+			break;
+		case CONTACT_NUMERO:
+			id = uri.getPathSegments().get(1);
+			ret = mDb.update(DB.Numero.TABLE_NAME, cont, "id" + " = " + id, null);
+			getContext().getContentResolver().notifyChange(CONTENT_URI_NUMERO, null);
+			break;
+		default:
+			throw new IllegalArgumentException("URI non support�e : "+uri);
+		}
+		return ret;
+	}*/
+	
+	public String getType(Uri uri) {
+		Log.i("Provider", "getType()");
+		switch (uriMatcher.match(uri)) {
+			case MOVIES:
+				return "vnd.android.cursor.dir/movies";
+			case MOVIE_ID:
+				return "vnd.android.cursor.item/movie";
+			default:
+				throw new IllegalArgumentException("Unsupportes URI : " + uri);
+		}
+	}
+	
+	public boolean exist(Uri uri) {
+		boolean ret = false;
+		Cursor c = query(uri, null, null, null, null);
+		if (c.getCount() > 0)
+			ret = true;
+		c.close();
+		return ret;
+	}
+}
